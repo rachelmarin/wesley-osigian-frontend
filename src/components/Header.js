c249f25f896b2cb03e49c56b2a7f2d6062484645
rachelmarin <Rachelmarinpal@gmail.com>
2021-07-15 16:03:51 -0400

    start up

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Author Wesley Osigian </h1>
+            </div>
+        )
+    }
+}
+export default Header
