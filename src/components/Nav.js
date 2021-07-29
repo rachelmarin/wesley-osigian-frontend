42866fec2c657a49342f0ba3f583b81e6d63c23d
rachelmarin <Rachelmarinpal@gmail.com>
2021-07-28 21:43:04 -0400

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,8 @@ class Nav extends Component {
             <nav className="navbar">
                 <ul>
                     <li><Link to="/"> Home </Link></li>
-                    <li><Link to="/"> About Wesley Osigian </Link></li>
-                    <li><Link to="/"> Books By Wesley </Link></li>
+                    <li><Link to="/About"> About Wesley Osigian </Link></li>
+                    <li><Link to="/Books"> Books By Wesley </Link></li>
                     
                 </ul>
             </nav>
