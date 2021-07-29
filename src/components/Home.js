b35046b849d96ae17ce1b9323c089bbcb7d3a1ec
rachelmarin <Rachelmarinpal@gmail.com>
2021-07-28 21:36:33 -0400

    created home component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+  
+const Home = () => {
+    
+    return (
+       <div>
+         <h1> Explore the work of Author Wesley Osigian </h1>
+         <p>A brilliant mind with stories that will captivate and astound you!</p>
+       
+       </div>
+    )
+}
+
+
+export default Home;
