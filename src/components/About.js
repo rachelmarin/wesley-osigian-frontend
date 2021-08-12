f0f27640df2cdb5a8bbe971af5b3a9aa1dde4792
rachelmarin <Rachelmarinpal@gmail.com>
2021-08-11 23:23:06 -0400

    description

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ class About extends Component {
     render() {
         return (
             <div>
-                
+               <p>Wesley Osigian is an American Author who finds an interesting way to look at the world and create his own worlds, he shares this with his readers</p>  
             </div>
         )
     }
